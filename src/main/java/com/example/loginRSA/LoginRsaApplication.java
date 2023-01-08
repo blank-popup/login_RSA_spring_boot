bde92217065e70c24f401437ca4a935e8a764d2b
blank-popup <ha6446@gmail.com>
2023-01-08 22:00:30 +0900

    00 start project

---
+++ src/main/java/com/example/loginRSA/LoginRsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginRSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginRSA/LoginRsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginRSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
