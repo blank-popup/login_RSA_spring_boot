2fe897c93cd251e92987672dc7914478ab92f880
blank-popup <ha6446@gmail.com>
2023-01-10 21:19:07 +0900

    02 use application.yml:

--- src/main/java/com/example/loginRSA/login/controller.java
+++ src/main/java/com/example/loginRSA/login/controller.java
@@ -1,6 +1,7 @@
 package com.example.loginRSA.login;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,19 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/")
 public class controller {
+    @Value("${str.common}")
+    private String strCommon;
+    @Value("${str.individual}")
+    private String strIndivudual;
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody ModelUser modelUser) {
-        log.trace("$$$$$ RestController trace login start");
-        log.debug("$$$$$ RestController debug login start");
-        log.info("$$$$$ RestController info login start");
-        log.warn("$$$$$ RestController warn login start");
-        log.error("$$$$$ RestController error login start");
+        log.trace("$$$$$ RestController trace login start {} {}", strCommon, strIndivudual);
+        log.debug("$$$$$ RestController debug login start {} {}", strCommon, strIndivudual);
+        log.info("$$$$$ RestController info login start {} {}", strCommon, strIndivudual);
+        log.warn("$$$$$ RestController warn login start {} {}", strCommon, strIndivudual);
+        log.error("$$$$$ RestController error login start {} {}", strCommon, strIndivudual);
         if (modelUser.getId() == 18) {
             log.info("$$$$$ RestController login success");
-            modelUser.setName("Impossible");
+            modelUser.setName("ImPossible");
         }
         else {
-            log.info("$$$$$ RestController login fialure");
+            log.warn("$$$$$ RestController login fialure");
             modelUser.setName("ImnotUser");
         }
         return ResponseEntity.status(HttpStatus.OK).body(modelUser);
